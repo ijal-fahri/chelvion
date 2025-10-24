098b8df2c60eddb868437c31dd610ace624c5673
ijal-fahri <aprizalf753@gmail.com>
2025-10-24 16:27:32 +0700

    mantapp

--- app/Http/Middleware/admin.php
+++ app/Http/Middleware/admin.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 class Admin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         if(Auth::user()->usertype != 'admin')
